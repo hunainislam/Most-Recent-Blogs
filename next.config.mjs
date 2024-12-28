dda3b45b1169d52d54b7e6b541f8d00173fd85ba
Hunain <hunainislam09@gmail.com>
2024-12-28 23:22:12 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-import 'dotenv/config';
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -10,12 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  },
 };
-
 
 export default nextConfig;
