a4c24bb411793406a94e9edeadcdc8b162f24697
Hunain <hunainislam09@gmail.com>
2024-12-28 23:10:23 +0500

    Push2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config();
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -8,6 +10,12 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
 };
 
+module.exports = nextConfig;
 export default nextConfig;
